4591ab49e69167a0a784e96612b9c81d09b5bb66
hetgondaliya <132036737+hetgondaliya40@users.noreply.github.com>
2024-06-27 16:46:48 +0530

    Update digital.dart

--- lib/screen/view/digital/digital.dart
+++ lib/screen/view/digital/digital.dart
@@ -16,6 +16,7 @@ class _DigitalClockState extends State<DigitalClock> {
   bool digitalSwitch = false;
   bool analogSwitch = false;
   bool strapSwitch = false;
+  bool stopSwitch = true;
 
   TimeOfDay time = TimeOfDay.now();
   DateTime dateTime = DateTime.now();
@@ -43,6 +44,40 @@ class _DigitalClockState extends State<DigitalClock> {
     "Nov",
     "Des"
   ];
+  int hh = 0;
+
+  int mm = 0;
+
+  int ss = 0;
+
+  bool isStop = true;
+
+  void clockTimer() async {
+    isStop = true;
+    await Future.delayed(const Duration(milliseconds: 100), () {
+      ss++;
+      if (isStop) {
+        ss++;
+      }
+      if (ss > 59) {
+        ss = 0;
+        mm++;
+      }
+      if (mm > 59) {
+        mm = 0;
+        hh++;
+      }
+      if (hh >= 12) {
+        hh = 0;
+      }
+      setState(() {});
+    });
+    if (isStop) {
+      clockTimer();
+    }
+  }
+
+  List emptyseaved = [];
 
   @override
   void initState() {
@@ -157,6 +192,33 @@ class _DigitalClockState extends State<DigitalClock> {
                 ),
                 child: Switch(
                   value: strapSwitch,
+                  onChanged: (val) {
+                    strapSwitch = val;
+                    setState(() {});
+                  },
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Text(
+                "04.",
+                style: TextStyle(
+                  fontSize: textScaler.scale(20),
+                ),
+              ),
+              title: Text(
+                "Stop Watch",
+                style: TextStyle(
+                  fontSize: textScaler.scale(22),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              trailing: Theme(
+                data: ThemeData(
+                  useMaterial3: true,
+                ),
+                child: Switch(
+                  value: stopSwitch,
                   onChanged: (val) {
                     strapSwitch = val;
                     setState(() {});
@@ -296,6 +358,202 @@ class _DigitalClockState extends State<DigitalClock> {
                 ],
               ),
             ),
+            Visibility(
+              visible: strapSwitch,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "${(time.hour % 12).toString().padLeft(2, '0')}:${(time.minute).toString().padLeft(2, '0')}",
+                            style: TextStyle(
+                              fontSize: textScaler.scale(50),
+                              color: const Color(0xff3f6080),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "\n${(time.hour < 12) ? 'AM' : 'PM'}",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: const Color(0xff3f6080),
+                              fontSize: textScaler.scale(12),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "${(dateTime.day).toString().padLeft(2, '0')}/${month[dateTime.month - 1]}   ",
+                        style: TextStyle(
+                          color: const Color(0xff3f6080),
+                          fontSize: textScaler.scale(20),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Transform.scale(
+                    scale: 9,
+                    child: CircularProgressIndicator(
+                      strokeWidth: 0.8,
+                      color: const Color(0xff3f6080),
+                      value: dateTime.second / 60,
+                    ),
+                  ),
+                  Transform.scale(
+                    scale: 8,
+                    child: CircularProgressIndicator(
+                      strokeWidth: 1.1,
+                      color: Colors.white54,
+                      value: dateTime.minute / 60,
+                    ),
+                  ),
+                  Transform.scale(
+                    scale: 7,
+                    child: CircularProgressIndicator(
+                      strokeWidth: 1.5,
+                      color: const Color(0xff3f6080),
+                      value: (time.hour % 12 + (time.minute / 60)) / 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Visibility(
+              visible: stopSwitch,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Transform.scale(
+                              scale: 5,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 1.5,
+                                value: ss / 60,
+                              ),
+                            ),
+                            Text(
+                              "${(hh).toString().padLeft(2, "0")} : ${(mm).toString().padLeft(2, "0")} : ${(ss).toString().padLeft(2, "0")}",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: textScaler.scale(25),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: h * 0.12,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton.icon(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xffEFF9FF),
+                                shadowColor: Color(0xff3f6080),
+                              ),
+                              onPressed: () {
+                                if (!isStop) {
+                                  clockTimer();
+                                }
+                                setState(() {});
+                              },
+                              icon: Icon(Icons.play_arrow),
+                              label: Text("Start"),
+                            ),
+                            ElevatedButton.icon(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xffEFF9FF),
+                                shadowColor: Color(0xff3f6080),
+                              ),
+                              onPressed: () {
+                                isStop = false;
+                                emptyseaved.add({
+                                  'hour': hh,
+                                  'min': mm,
+                                  'sec': ss,
+                                });
+                                setState(() {});
+                              },
+                              icon: Icon(Icons.play_arrow),
+                              label: Text("Stop"),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: h * 0.02,
+                        ),
+                        ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xffEFF9FF),
+                            shadowColor: Color(0xff3f6080),
+                          ),
+                          onPressed: () {
+                            isStop = false;
+                            hh = mm = 0;
+                            ss = 0;
+                            emptyseaved = [];
+                            setState(() {});
+                          },
+                          icon: Icon(Icons.restart_alt),
+                          label: Text("Restart"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          ...emptyseaved.map(
+                            (e) => Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Row(
+                                // crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Hour : ${e['hour']} : ",
+                                    style: TextStyle(
+                                        fontSize: 15, color: Colors.white),
+                                  ),
+                                  Text("Min : ${e['minute']} : "),
+                                  Text("Sec : ${e['seconds']}"),
+                                  SizedBox(
+                                    width: w * 0.01,
+                                  ),
+                                  IconButton(
+                                      onPressed: () {
+                                        emptyseaved.remove(e);
+                                        setState(() {});
+                                      },
+                                      icon: Icon(
+                                        Icons.delete,
+                                        size: 25,
+                                      )),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
             // SizedBox(height:  h * 0.1,),
             // Padding(
             //   padding: const EdgeInsets.only(top: 590.0),
